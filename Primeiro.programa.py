a81b84627f21cc6c2cc5db158e6545f7f2f9a19f
Max-mcp <duartemaxwell28@gmail.com>
2026-06-12 21:21:50 -0300

    come~cando a printar

---
+++ Primeiro.programa.py
@@ -0,0 +1,6 @@
+nome = "Maxwell"  # str - String
+Ultimo_nome = "Duarte"#str -String
+Idade = 22 # int - inteito
+maior_de_idade = True  # bool : booleano: True/False
+print("olá meu", nome, "você tem",Idade, "Anos")
+print(f"Olá , {nome} {Ultimo_nome} Você tem {Idade}, vocé é maior de idade? {maior_de_idade}")
