21dbe54803a6a22d4589688d9ece83e96f4a3d8c
Max-mcp <duartemaxwell28@gmail.com>
2026-06-16 21:29:12 -0300

    max

---
+++ casar ou bike.py
@@ -0,0 +1,20 @@
+# Casar ou Comprar uma Bicicleta
+
+print("casar ou comprar uma bicilcleta?")
+
+resposta = input("Você está Gordos s/n")
+
+if resposta == "s":
+    quer_emagrecer = input("Você quer emagrecer?")
+    if quer_emagrecer == "S":
+        print("Comprar uma bicicleta")
+    else:
+        print("Então Case")
+else:
+    quer_engordar = input("Você quer engordar?")
+    if quer_engordar == "s":
+        print("Então case!")
+    else:
+        print("Compre uma bicicleta")
+           
+
