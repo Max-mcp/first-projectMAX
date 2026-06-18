e629d40754b33c73985df2a1fc05daaea9726801
Max-mcp <duartemaxwell28@gmail.com>
2026-06-17 21:14:01 -0300

    descobrindo numeros impares e pares

---
+++ Par ou Impar.py
@@ -0,0 +1,8 @@
+#Par ou impar
+
+numero= int(input("digite o numero:"))
+
+if numero % 2 == 0 :
+    print("esse numero é par")
+else:
+    print("Este número é impar")
--- mediaaluno.py
+++ mediaaluno.py
@@ -4,7 +4,22 @@ nota3=float(input("digite sua terceira nota"))
 
 media= nota1 + nota2 + nota3 / 3
 
-if media >= 7
+if media >= 7 :
     print(f"Aluno aprovado com media: {media: .2f}")
+elif media >=3 and media < 7 :
+    print(f"Aluno (a) em recuperação com média :{media: .2f}")
+    fez_recuperacao = input("Aluno já fez a recuperação? s/n")
+    if fez_recuperacao == "s":
+        nota_de_recuperacao = float(input("digite a nota de recuperação:"))
+        if nota_de_recuperacao >=5:
+            print("Aluno aprovado pela recuperação")
+        else:
+            print("Aluno não obteve nota suficiente para ser aprovado após a recuperação")
+    else:
+        print("Aluno continua de Recuperação")
+else:
+    print(f"Aluno reprovado com média {media: .2f}")
+
+
     
 
